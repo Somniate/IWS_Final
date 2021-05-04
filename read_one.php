f50c0265790cca70877a023ca1ee4ef98a4a7050
CritCheese <69691181+CritCheese@users.noreply.github.com>
2021-05-04 08:13:47 +0700

    Update read_one.php

--- read_one.php
+++ read_one.php
@@ -36,6 +36,33 @@ $article = $result->fetch_assoc();
         <!-- content -->
         <p><?php echo $article['content_article'] ?></p>
     </div>
+    
+    <!-- comment section -->
+    <section class="comments">
+        <div class="container">
+            <h3>Comments</h3>
+            <?php 
+            $id_a = $article['id_article'];
+            $sql_comment = "SELECT * FROM comments WHERE id_article = $id_a";
+            $result_comment = $mysqli->query($sql_comment);
+            $comments = [];
+            if ($result_comment) {
+                $comments = $result_comment->fetch_all(MYSQLI_ASSOC);
+            }
+            ?>
+            <?php if (count($comments) == 0) : ?>
+                    <p>No comments</p>
+                <?php else : ?>
+                    <?php foreach ($comments as $row) : ?>
+                        <div>
+                            <h2><?php echo $row['author_comment'] ?></a></h2>
+                            <p><?php echo $row['date_created'] ?></p>
+                            <p><?php echo $row['content_comment'] ?></p>
+                        </div>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+        </div>
+    </section>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
